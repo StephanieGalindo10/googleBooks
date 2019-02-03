7a5616292ed8a2f545ce244fd055c9f5f2eeea02
stephaniegalindo10 <stephanimatedworld@gmail.com>
2019-02-03 12:58:06 -0800

    testing

---
+++ client/build/precache-manifest.2b82c5edbf207f1333ce4164b6238044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7c363e15a2287048b9e",
+    "url": "/static/js/main.f7c363e1.chunk.js"
+  },
+  {
+    "revision": "70d5445ab58e6b834b25",
+    "url": "/static/js/1.70d5445a.chunk.js"
+  },
+  {
+    "revision": "f7c363e15a2287048b9e",
+    "url": "/static/css/main.72d72eba.chunk.css"
+  },
+  {
+    "revision": "7a4ff817254a5b0b38b5ad10aea89854",
+    "url": "/index.html"
+  }
+];
